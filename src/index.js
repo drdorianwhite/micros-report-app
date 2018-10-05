6c09f1d0558c02a412c1744b32ed9c4f36603161
Lehel Dorian Kovach <drdorianwhite@gmail.com>
2018-10-05 05:18:43 -0700

    fgju

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const element = <h1>Micros Project</h1>;
+ReactDom.render(element, document.getElementById("root"));
